3868da0a5ebb7f4b270224ac196d8452710336e2
Joe <JoeFrew95@gmail.com>
2015-01-04 22:13:06 +0000

    changed around files in filesystem, and added {{STATIC_URLS}} tags in templates

--- PortfolioWebsite/PortfolioWebsite/settings.py
+++ PortfolioWebsite/PortfolioWebsite/settings.py
@@ -81,9 +81,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/s/'
+STATIC_ROOT = os.path.join(BASE_DIR, '.static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),# Put strings here, like "/home/html/static" or "C:/www/django/static".
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'homepage/static'),
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.""
 )
--- PortfolioWebsite/homepage/views.py
+++ PortfolioWebsite/homepage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
+from django.template import RequestContext, Context
 # The homepage view, simply renders the homepage template and returns
-def homepage(e):
-    return render_to_response("homepage/main5.html", {})
+def homepage(request):
+    return render_to_response("homepage/main.html", context_instance=RequestContext(request))
