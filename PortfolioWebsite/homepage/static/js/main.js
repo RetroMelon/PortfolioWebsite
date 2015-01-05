1dd88da273813bffd05292bcb4200db1c0efe3b7
Joe <JoeFrew95@gmail.com>
2015-01-05 02:20:34 +0000

    added the real data to the template. updated filters, made some other visual tweaks.

--- PortfolioWebsite/homepage/static/js/main.js
+++ PortfolioWebsite/homepage/static/js/main.js
@@ -3,16 +3,41 @@
 $(document).ready(function(){
   var $container = $('.item-container');
   var $noitems = $('#noitems');
-  var displaynoitems = false;
+  var $invisiblesmoothscroll = $('#ninvisiblesmoothscroll');
+  var lastheight = $container.height();
 
+  //decides whether to display the "There are no items" text based on
+  //how many items there are. also decides whether to animate the height
+  //the invisible div, so we don't have an uncomfortable jerky motion
   function decideNoItems() {
+    //getting number of items and deciding what to do.
     var numberofitems = $container.find(".active").size();
+    console.log("no. items: " + numberofitems);
 
+    //if the number of items is zero, show the message and smooth scroll
     if(numberofitems <= 0) {
+      //showing the message
       $noitems.css("display", "block");
+
+      //smoothscrolling
+      //checking if height decreased, and by how much
+      var newheight = $container.height();
+      var heightdif = lastheight - newheight; //+ve if smaller, -ve if bigger
+      console.log('newheight' + newheight + '  lastheight ' + lastheight);
+      console.log('height dif ' + heightdif);
+
+      if(heightdif > 0) {
+        $('#invisiblesmoothscroll').height(heightdif);
+        console.log("animating!");
+        $('#invisiblesmoothscroll').animate({height: '1px'}, 550);
+      }
+
     } else {
       $noitems.css("display", "none");
     }
+
+    //setting the most recent height of the container
+    lastheight = $container.height();
   }
 
   $container.isotope({
@@ -26,12 +51,14 @@ $(document).ready(function(){
   //the click listener for the filter buttons
   function filterClick(data){
     $(filters).css("opacity", 0.6);
+    $(filters).css("background-color", "");
     $(data.target).css("opacity", 1);
 
     var filterText = "";
 
     if($(data.target).attr("class") == "btn") {
       $(filters[filters.length - 1]).val($(data.target).attr("filter"));
+      $(data.target).css("background-color", "#346A6C"); //only set the bg-color if a button
     }
 
     $(filters[filters.length - 1]).trigger('keyup');
